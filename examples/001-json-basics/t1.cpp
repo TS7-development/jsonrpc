4a410cb6064a9bdf8e010e5c2d9b49586d2e1e13
Tarek Schwarzinger <tarek.schwarzinger@googlemail.com>
2021-06-04 23:58:13 +0200

    Examples  - Added documentation to JSON basics

--- examples/001-json-basics/t1.cpp
+++ examples/001-json-basics/t1.cpp
@@ -2,6 +2,21 @@
 
 #include <boost/json.hpp>
 
+/**
+ * @brief Basic object update
+ *
+ * Updating the originally create parameter object and check, if it will be updated in the request as well by
+ * internally knowing it's parent.
+ *
+ * This is not the case, so unfortunately this hasn't worked.
+ *
+ * @param request The request object.
+ * @param params The parameter object.
+ *
+ * @since 1.0
+ *
+ * @author Tarek Schwarzinger <tarek.schwarzinger@googlemail.com>
+ */
 void t1(boost::json::object& request, boost::json::object& params) {
   params["value1"] = 7;
   std::cout << request << std::endl;
--- examples/001-json-basics/t3.cpp
+++ examples/001-json-basics/t3.cpp
@@ -2,6 +2,17 @@
 
 #include <boost/json.hpp>
 
+/**
+ * @brief Convert to string
+ *
+ * Simple test to see how to convert a JSON object to a string, that can be used for later transmission.
+ *
+ * @param request Reference to the request object.
+ *
+ * @since 1.0
+ *
+ * @author Tarek Schwarzinger <tarek.schwarzinger@googlemail.com>
+ */
 void t3(boost::json::object& request) {
   std::stringstream ss;
   ss << request;
