e2416bf350a07729f10da78d1338e45d3aa7170f
Tarek Schwarzinger <tarek.schwarzinger@googlemail.com>
2021-09-28 20:42:51 +0200

    Added support for void return type of procedures.

--- jsonrpc/procedure.hpp
+++ jsonrpc/procedure.hpp
@@ -46,5 +46,46 @@ namespace ts7 {
         Response<TId, TRet> response;
         Error<TId> error;
     };
+
+    template <typename TId, typename... TArgs>
+    class Procedure<TId, void, TArgs...> {
+      public:
+        using callback_t = std::function<void(TArgs...)>;
+        using handler_failure = typename RequestHandler<TId, void, TArgs...>::maybe_failed;
+
+        template <typename... UArgs>
+        Procedure(callback_t callback, UArgs... args)
+          : handler([callback](const TId&, TArgs... args) -> handler_failure {
+              try {
+                callback(args...);
+                return handler_failure();
+              }
+              catch(error::Exception& e) {
+                return e.ec;
+              }
+              catch(std::exception& e) {
+                return error::ErrorCode(static_cast<std::int32_t>(error::ErrorCodes::INTERNAL_ERROR), e.what());
+              }
+            }, args...)
+        {}
+
+        boost::json::object operator()(const boost::json::object& request) {
+          TId id;
+
+          handler_failure state = handler(request, id);
+          if (state) {
+            return response(id);
+          }
+          else {
+            const error::ErrorCode ec = state.getFailed();
+            return error(id, ec);
+          }
+        }
+
+      protected:
+        RequestHandler<TId, void, TArgs...> handler;
+        Response<TId, void> response;
+        Error<TId> error;
+    };
   }
 }
